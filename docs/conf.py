2c5240b9c9e6262c86a7debef2895e0451a7e36e
MiekeNel <98015730+MiekeNel@users.noreply.github.com>
2023-10-30 22:14:02 +0200

    Docstrings and Sphinx Documentation updated
    First batch of project documentation, with docstrings Sphinx Autodoc feature updated.

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'mySite'
 copyright = '2023, Mieke Nel'
 author = 'Mieke Nel'
@@ -15,24 +10,21 @@ import os
 import sys
 import django
 
+# Add the parent directory of your Django project to the sys.path.
 sys.path.insert(0, os.path.abspath('..'))
+
+# Set the DJANGO_SETTINGS_MODULE to your Django project's settings.
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mySite.settings'
+
+# Initialize Django to work with autodoc.
 django.setup()
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ['sphinx.ext.autodoc',
-'sphinx.ext.viewcode',
-'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
